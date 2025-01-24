5d9d62004305a2bb7a5c1317533b0e0940d2d173
Anshika-1209 <anshikaioc1209@gmail.com>
2025-01-24 16:51:01 +0530

    Solution to Input and Print a Character

--- Input and Print a Character@@6788d5012db25efec946f93b/code.c
+++ Input and Print a Character@@6788d5012db25efec946f93b/code.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-#include <ctype.h>
-int main()
-{
-    char ch;
-    scanf("%c",&ch);
-    ch=tolower(ch);
-    printf("You entered:%c\n",ch);
-    return 0;
+#include<stdio.h>
+int main(){
+    char a;
+    printf("a");
+    scanf("%c",&a);
 }
