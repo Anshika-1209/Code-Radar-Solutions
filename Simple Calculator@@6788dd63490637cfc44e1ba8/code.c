202ca6aacc5b588842fe2e23b08f2889a2fcbb31
Anshika-1209 <anshikaioc1209@gmail.com>
2025-02-05 00:18:54 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    char operator;
+    scanf("%d%d",&a,&b);
+    scanf("%c",&operator);
+    printf(" ");
+    else
+    {
+        printf("Error");
+    }
+}
